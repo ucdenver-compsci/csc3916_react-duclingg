fa193694caeb1ca6fcb38dd704cdb948d5282ec7
Justin Hoang <justin.lhoang3@gmail.com>
2024-04-15 17:23:19 -0600

    fix moviedetail.js

--- src/components/moviedetail.js
+++ src/components/moviedetail.js
@@ -38,7 +38,7 @@ class MovieDetail extends Component {
                         <ListGroupItem><h4><BsStarFill/> {this.props.selectedMovie.avgRating}</h4></ListGroupItem>
                     </ListGroup>
                     <Card.Body>
-                        {this.props.selectedMovie.reviews.map((review, i) =>
+                        {this.props.selectedMovie.movie_reviews.map((review, i) =>
                             <p key={i}>
                                 <b>{review.username}</b>&nbsp; {review.review}
                                 &nbsp;  <BsStarFill /> {review.rating}
