fc55c9693e4630d01b5880d8aeb1fc91bdf001fe
Justin Hoang <justin.lhoang3@gmail.com>
2024-04-15 23:46:18 -0600

    fix movieActions.js

--- src/actions/movieActions.js
+++ src/actions/movieActions.js
@@ -73,7 +73,7 @@ export function fetchMovies() {
 
 export function saveReview(movieId, reviewData) {
     return dispatch => {
-        return fetch(`${env.REACT_APP_API_URL}/reviews`, {
+        return fetch(`${env.REACT_APP_API_URL}/${movieId}/reviews`, {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
